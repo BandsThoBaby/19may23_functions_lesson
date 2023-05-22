86477b9cd37d194d90d865b810147a0c97f468a2
BandsThoBaby <32070633+BandsThoBaby@users.noreply.github.com>
2023-05-22 17:31:51 +0100

    Add files via upload
    A Python function to calculate the square of a number and return additional values.

---
+++ 19may23_functions_lesson.py
@@ -0,0 +1,8 @@
+import random
+def squared(g):
+    number = g * g
+    letter = 7 * 7
+    tree = g * 3
+    return number, letter, tree
+lesson2, lesson3, lesson4 = squared(20)
+print(lesson2, lesson3, lesson4)
